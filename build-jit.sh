f904c385747a7a653d2f567e111f2b51bf1b1101
Wengao Ye <elleryqueenhomels@hotmail.com>
2021-02-16 01:23:54 +0800

    Update shell script output file name

--- build-jit.sh
+++ build-jit.sh
@@ -1,1 +1,1 @@
-clang++ -g -std=c++17 -stdlib=libc++ src/lexer.cpp src/parser.cpp src/codegen.cpp src/main.cpp `/usr/local/opt/llvm/bin/llvm-config --cppflags --ldflags --system-libs --libs core orcjit native` -o kc-jit.app
+clang++ -g -std=c++17 -stdlib=libc++ src/lexer.cpp src/parser.cpp src/codegen.cpp src/main.cpp `/usr/local/opt/llvm/bin/llvm-config --cppflags --ldflags --system-libs --libs core orcjit native` -o ksc-jit.app
